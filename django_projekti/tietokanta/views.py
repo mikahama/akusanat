ca9cd98ad5af1abac3b334e0c2c961da23724084
Mika Hämäläinen <mika.hamalainen@mail.com>
2016-04-21 20:40:55 +0300

    xml output

--- django_projekti/tietokanta/views.py
+++ django_projekti/tietokanta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 import tietokanta.mongoilija as mongoilija
+from django.template.defaulttags import register
 # Create your views here.
 
 file_types = {
@@ -15,10 +16,106 @@ def xml_out(request):
     lemmas = mongoilija.get_all_lemmas(language)
 
     template_file = file_types[language][xml_type]
-
     template = loader.get_template(template_file)
     context = RequestContext(request, {
         'lemmas': lemmas,
         "file_name" : xml_filename
     })
     return HttpResponse(template.render(context))
+
+@register.assignment_tag
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+@register.assignment_tag
+def get_item_list(dictionary, key):
+    lista = dictionary.get(key)
+    if lista is None:
+        return []
+    else:
+        return lista
+
+@register.assignment_tag
+def get_item_dict(dictionary, key):
+    lista = dictionary.get(key)
+    if lista is None:
+        return {}
+    else:
+        return lista
+
+@register.assignment_tag
+def has_key(dictionary, key):
+    if dictionary.get(key) is not None:
+        return True
+    else:
+        return False
+
+@register.assignment_tag
+def has_subkey(dictionary, key):
+    keys = key.split(".")
+    first_dict = dictionary.get(keys[0])
+    if first_dict is not None:
+        second_dict = first_dict.get(keys[1])
+        if second_dict is None:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+@register.assignment_tag
+def filename_and_pos(pos, file_name):
+    return pos + "_finsms.xml" == file_name
+
+@register.assignment_tag
+def filename_and_pos_sms(pos, file_name):
+    return pos + "_sms2X.xml" == file_name
+
+
+@register.assignment_tag
+def get_subitem(dictionary, key):
+    keys = key.split(".")
+    first_dict = dictionary.get(keys[0])
+    if first_dict is not None:
+        value = first_dict[keys[1]]
+        return value
+    else:
+        return None
+@register.assignment_tag
+def get_subitem_list(dictionary, key):
+    keys = key.split(".")
+    first_dict = dictionary.get(keys[0])
+    if first_dict is not None:
+        value = first_dict.get(keys[1])
+        if value is None:
+            return []
+        else:
+            return value
+    else:
+        return []
+
+@register.assignment_tag
+def get_subitem_dict(dictionary, key):
+    keys = key.split(".")
+    first_dict = dictionary.get(keys[0])
+    if first_dict is not None:
+        value = first_dict.get(keys[1])
+        if value is None:
+            return {}
+        else:
+            return value
+    else:
+        return {}
+
+@register.assignment_tag
+def get_subitem_text(dictionary, key):
+    keys = key.split(".")
+    try:
+        first_dict = dictionary[keys[0]]
+        if first_dict is not None:
+            value = first_dict[keys[1]]
+            return value
+        else:
+            return ""
+    except:
+        return ""
