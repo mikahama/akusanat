4fe3dbd68517747ed3ef91ab769ae88d85a86780
Mika Hämäläinen <mika.hamalainen@helsinki.fi>
2016-09-23 12:32:42 +0300

    ide

--- lataa_xml.py
+++ lataa_xml.py
@@ -1,12 +1,14 @@
 import urllib2
 import urllib
-url = "https://victorio.uit.no/langtech/trunk/langs/sms/src/morphology/stems/"
+#izh https://victorio.uit.no/langtech/trunk/langs/izh/src/morphology/stems/
+url = "https://victorio.uit.no/langtech/trunk/ped/sms/src/"
 response = urllib2.urlopen(url)
 html = response.read()
 from BeautifulSoup import BeautifulSoup
 soup = BeautifulSoup(html)
 for li in soup.findAll('li'):
 	dl_url = li.a.get('href')
+	print "Downloading ", dl_url
 	if dl_url == "../":
 		pass
 	else:
