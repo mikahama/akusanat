5b1d29d75e26ef3a148721c06a2c68c82805088f
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2019-03-08 18:12:09 +0200

    ..

--- django_projekti/upload_to_server.sh
+++ django_projekti/upload_to_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm Archive.zip
-zip -r Archive.zip manage.py update_smsxml.sh transducers/* saame/* tietokanta/*
+zip -r Archive.zip manage.py update_smsxml.sh saame/* tietokanta/*
 
 scp Archive.zip root@mikakalevi.com:/var/www/html/downloads/sms_xml.zip
